d25f976526c3f323fa8a26431da043a3f96b9c38
Robert Spatz <bspatz2@huskers.unl.edu>
2021-08-28 04:46:22 +0000

    added my major to the output

--- hello/hello.c
+++ hello/hello.c
@@ -4,7 +4,7 @@
  * Date: 2021/08/24
  *
  * A simple hello world program in C
- *
+ * partner: Bobby Spatz
  */
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,6 +13,7 @@ int main(int argc, char **argv) {
 
   printf("Logan!\n");
   printf("Computer Science\n")
-
+  printf("Bobby!\n");
+  printf("Computer Science!\n");
   return 0;
 }
